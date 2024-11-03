33fae3445da3d98f70cc3f2494114bcfbf40c2fc
bread <2797257510@qq.com>
2024-11-03 15:00:41 +0800

    1

--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
+
+type act struct {
+	put string
+	pht string
+}
 
 func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/**/*")
 	r.GET("admin/news", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "news.html", gin.H{
-			"title": "首页656111165",
-			"newt":  []interface{}{},
+			"title": "首页",
+			"newt": []interface{}{
+				&act{
+					put: "新闻标题111",
+					pht: "新闻标题222",
+				},
+			},
 		})
 	})
 	r.Run()
