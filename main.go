7f7b70f161c24fe6e9f3e6e136ba32cd0eef7fa6
bread <2797257510@qq.com>
2024-11-03 14:16:32 +0800

    main.go 删除了act

--- main.go
+++ main.go
@@ -5,23 +5,13 @@ import (
 	"net/http"
 )
 
-type act struct {
-	put string
-	pht string
-}
-
 func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/**/*")
 	r.GET("admin/news", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "news.html", gin.H{
 			"title": "首页",
-			"newt": []interface{}{
-				&act{
-					put: "新闻标题111",
-					pht: "新闻标题222",
-				},
-			},
+			"newt": []interface{}{},
 		})
 	})
 	r.Run()
