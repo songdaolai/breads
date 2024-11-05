9a861dcea9b12b83ea741f5ed0c97705034f63c3
bread <2797257510@qq.com>
2024-11-05 19:52:13 +0800

    帮我看看吧哥

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,12 +13,20 @@ type act struct {
 	Pht string
 }
 
+func Prithen(str1 string, str2 string) string {
+	fmt.Println(str1, str2)
+	return str1 + "---" + str2
+}
 func main() {
 	r := gin.Default()
+	r.SetFuncMap(template.FuncMap{
+		"Prithee": Prithen,
+	})
 	r.LoadHTMLGlob("templates/**/*")
 	r.GET("admin/news", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "news.html", gin.H{
 			"title": "首页",
+			"but":   "新闻",
 			"news": &act{
 				Put: "新闻标题222",
 				Pht: "新闻标题333",
