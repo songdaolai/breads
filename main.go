7ec45534dcff64e03415822c5698f6c15c00e43e
bread <2797257510@qq.com>
2024-11-09 18:53:07 +0800

    哥我这是哪里出粗错了啊

--- main.go
+++ main.go
@@ -1,39 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-type act struct {
-	Put string
-	Pht string
-}
-
-func Prithen(str1 string, str2 string) string {
-	fmt.Println(str1, str2)
-	return str1 + "---" + str2
-}
 func main() {
 	r := gin.Default()
-	r.SetFuncMap(template.FuncMap{
-		"Prithee": Prithen,
-	})
-	r.LoadHTMLGlob("templates/**/*")
-	r.Static("/css", "./static/css")
-	r.GET("admin/news", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "news.html", gin.H{
-			"title": "首页",
-			"but":   "新闻",
-			"news": &act{
-				Put: "新闻标题222",
-				Pht: "新闻标题333",
-			},
-		})
 
+	r.LoadHTMLGlob("templates/**/*")
+	r.GET("/user", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "default/user.html", gin.H{})
+	})
+
+	r.POST("/doAddUser", func(c *gin.Context) {
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+		c.JSON(http.StatusOK, gin.H{
+			"username": username,
+			"password": password,
+		})
 	})
 	r.Run()
 }
